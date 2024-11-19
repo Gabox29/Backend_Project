4937cbff1a1eb6adf51b14ce35e76d352509dbaf
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-11-19 22:41:53 +0100

    create user endpoint

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,1 +1,15 @@
 const { User } = require("../models/index");
+
+const UserController = {
+  async create(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.status(201).send({ message: "User created successfully", user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Something is wrong", error });
+    }
+  },
+};
+
+module.exports = UserController;
--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use("/users", require("./routes/users"));
+
 app.listen(PORT, () => console.log(`Server live on port: ${PORT}`));
