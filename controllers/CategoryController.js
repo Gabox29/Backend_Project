925daec1d9e4b5c57a52529df7381c572e40cdf7
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-11-27 00:52:55 +0100

    create both category getby endpoints

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,4 +1,5 @@
-const { Category } = require("../models/index");
+const { Category, Product, Sequelize  } = require("../models/index");
+const { Op } = Sequelize;
 
 const CategoryController = {
   async create(req, res) {
@@ -24,19 +25,60 @@ const CategoryController = {
       res.status(500).send({ message: "Something is wrong", error });
     }
   },
-  async delete(req,res) {
+  async delete(req, res) {
     try {
       await Category.destroy({
-        where:{
-          id: req.params.id
-        }
-      })
-      res.send({message:`Category with id ${req.params.id} deleted`})
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.send({ message: `Category with id ${req.params.id} deleted` });
     } catch (error) {
       console.error(error);
       res.status(500).send({ message: "Something is wrong", error });
     }
-  }
+  },
+  async getById(req, res) {
+    try {
+      const category = await Category.findByPk(req.params.id, {
+        attributes: ["id", "description"],
+      });
+      res.send(category);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Something is wrong", error });
+    }
+  },
+  async getByDescription(req, res) {
+    try {
+      const categories = await Category.findAll({
+        attributes: ["id", "description"],
+        where: {
+          description: {
+            [Op.like]: `%${req.params.description}%`,
+          },
+        },
+      });
+      res.send(categories);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Something is wrong", error });
+    }
+  },
+  async getAll(req, res) {
+    try {
+      const categories = await Category.findAll({
+        include: {
+          model: Product,
+          attributes: ["description", "price"],
+        },
+      });
+      res.status(200).send(categories);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Something is wrong", error });
+    }
+  },
 };
 
 module.exports = CategoryController;
--- routes/categories.js
+++ routes/categories.js
@@ -5,7 +5,8 @@ const router = express.Router();
 router.post("/", CategoryController.create);
 router.put("/id/:id", CategoryController.update);
 router.delete("/id/:id", CategoryController.delete);
-
-
+router.get("/getAll", CategoryController.getAll);
+router.get("/id/:id", CategoryController.getById);
+router.get("/description/:description", CategoryController.getByDescription);
 
 module.exports = router;
