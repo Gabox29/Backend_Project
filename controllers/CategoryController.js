b81e446633274a4694dd9e8fdedb0b31f1da7aa9
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-11-27 00:30:02 +0100

    update and delete Category endpoints

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -10,6 +10,33 @@ const CategoryController = {
       res.status(500).send({ message: "Something is wrong", error });
     }
   },
+  async update(req, res) {
+    try {
+      await Category.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      const category = await Category.findByPk(req.params.id);
+      res.send({ message: "Category updated successfully", category });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Something is wrong", error });
+    }
+  },
+  async delete(req,res) {
+    try {
+      await Category.destroy({
+        where:{
+          id: req.params.id
+        }
+      })
+      res.send({message:`Category with id ${req.params.id} deleted`})
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Something is wrong", error });
+    }
+  }
 };
 
 module.exports = CategoryController;
--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,10 @@ const express = require("express");
 const CategoryController = require("../controllers/CategoryController");
 const router = express.Router();
 
-router.post("/create", CategoryController.create);
+router.post("/", CategoryController.create);
+router.put("/id/:id", CategoryController.update);
+router.delete("/id/:id", CategoryController.delete);
+
+
 
 module.exports = router;
