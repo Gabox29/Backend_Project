8e9355dc5b8e3bbf89dc7158d31cbfc250d39295
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-11-27 05:31:28 +0100

    insert authentications in products CRUD

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const ProductController = require("../controllers/ProductController");
+const { authentication } = require("../middlewares/authentication");
 const router = express.Router();
 
-router.post("/", ProductController.create);
-router.put("/id/:id", ProductController.update);
-router.delete("/id/:id", ProductController.delete);
+router.post("/", authentication, ProductController.create);
+router.put("/id/:id", authentication, ProductController.update);
+router.delete("/id/:id", authentication, ProductController.delete);
 router.get("/getAll", ProductController.getAll);
 router.get("/id/:id", ProductController.getById);
 router.get("/description/:description", ProductController.getByDescription);
