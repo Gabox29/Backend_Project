beed098951bc3df0d92855347b0afd2671e4af9d
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-11-27 05:45:43 +0100

    insert isAdmin in products CRUD

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const ProductController = require("../controllers/ProductController");
-const { authentication } = require("../middlewares/authentication");
+const { authentication, isAdmin } = require("../middlewares/authentication");
 const router = express.Router();
 
-router.post("/", authentication, ProductController.create);
-router.put("/id/:id", authentication, ProductController.update);
-router.delete("/id/:id", authentication, ProductController.delete);
+router.post("/", authentication, isAdmin, ProductController.create);
+router.put("/id/:id", authentication, isAdmin, ProductController.update);
+router.delete("/id/:id", authentication, isAdmin, ProductController.delete);
 router.get("/getAll", ProductController.getAll);
 router.get("/id/:id", ProductController.getById);
 router.get("/description/:description", ProductController.getByDescription);
