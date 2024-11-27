d3e1309fd24402ecea3337ed0a294e20378ebf39
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-11-27 05:23:57 +0100

    create user validation

--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -14,16 +12,57 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(models.Review);
     }
   }
-  User.init({
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING,
-    birth: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false, 
+        validate: {
+          notEmpty: { msg: "First name cannot be empty" }, 
+        },
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Last name cannot be empty" },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Email cannot be empty" },
+          isEmail: { msg: "Must be a valid email address" }, 
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Password cannot be empty" },
+        },
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Role cannot be empty" },
+        },
+      },
+      birth: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Birth date cannot be empty" },
+          isDate: { msg: "Must be a valid date" }, 
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
