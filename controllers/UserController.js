5be2d1a7ef9a333af516e3202865e15e8bf43760
Gabriel Gonzalez <gabrielgonzgils@gmail.com>
2024-11-27 04:34:41 +0100

    create User login endpoint with bscrypt and JWT

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,7 @@
-const { User } = require("../models/index");
-const bcrypt = require ('bcryptjs');
+const { User, Token } = require("../models/index");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/config.json")["development"];
 
 const UserController = {
   async create(req, res) {
@@ -11,6 +13,23 @@ const UserController = {
       console.error(error);
       res.status(500).send({ message: "Something is wrong", error });
     }
+  },
+  async login(req, res) {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!user) {
+      return res.status(400).send({ message: "Incorrect email or password" });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(400).send({ message: "Incorrect email or password" });
+    }
+    let token = jwt.sign({ id: user.id }, jwt_secret);
+    await Token.create({ token, UserId: user.id }); //guardar Token en la tabla Tokens
+    res.send({ token, message: "Successfully logged", user });
   },
 };
 
